9dd5a297d0d25c872f1b0232a37399325b2cfcbd
Doniyor-Roziqulov <doniyorruziqulov33@gmail.com>
2024-10-08 23:40:55 +0500

    Add loading

--- src/app/(layout)/shop/[id]/page.jsx
+++ src/app/(layout)/shop/[id]/page.jsx
@@ -10,22 +10,25 @@ const Detail = async ({ params }) => {
         <section className="pt-9 pb-20">
             <div className="container mx-auto max-w-[1330px] px-5">
                 <div className="flex items-start justify-between">
-                    <div className="w-1/2">
+                    <div className="w-1/2 flex gap-x-[30px]">
                         {data?.images?.length > 1 ? (
-                            <div className="flex">
+                            <div className="flex flex-col gap-y-5">
                                 <Image
+                                    className="object-contain w-20 h-20 bg-[#F9F1E7] rounded-[10px]"
                                     src={data?.images[0]}
                                     alt="photo"
                                     width="80"
                                     height="80"
                                 />
                                 <Image
+                                    className="object-contain w-20 h-20 bg-[#F9F1E7] rounded-[10px]"
                                     src={data?.images[1]}
                                     alt="photo"
                                     width="80"
                                     height="80"
                                 />
                                 <Image
+                                    className="object-contain w-20 h-20 bg-[#F9F1E7] rounded-[10px]"
                                     src={data?.images[2]}
                                     alt="photo"
                                     width="80"
@@ -35,9 +38,9 @@ const Detail = async ({ params }) => {
                         ) : (
                             <></>
                         )}
-                        <div className="bg-[#F9F1E7] w-[423px] rounded-[10px]">
+                        <div className="bg-[#F9F1E7] w-[423px] h-[500px] rounded-[10px]">
                             <Image
-                                className="object-contain"
+                                className="object-contain w-[423px] h-[500px]"
                                 width="423"
                                 height="500"
                                 src={data?.images[0]}
--- src/app/(layout)/shop/loading.js
+++ src/app/(layout)/shop/loading.js
@@ -1,7 +1,17 @@
 import React from "react";
+import "@/app/(layout)/shop/loading.css";
 
 const Loading = () => {
-    return <div>loading</div>;
+    return (
+        <div className="flex items-center justify-center min-h-screen">
+            <div className="loader">
+                <div className="circle"></div>
+                <div className="circle"></div>
+                <div className="circle"></div>
+                <div className="circle"></div>
+            </div>
+        </div>
+    );
 };
 
 export default Loading;
